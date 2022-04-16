7be2454cc13c8ddb1605e235261eb84337df6ebb
khadada <lkhadada@gmail.com>
2022-04-16 07:31:11 +0100

    Create initialize_ui method

--- main.py
+++ main.py
@@ -3,3 +3,19 @@ import sys
 from PyQt5.QtWidgets import (QApplication, QMainWindow, QGridLayout,QWidget)
 from custom_widget_placeholder import Color
 
+# Class of our GUI:
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.initialize_ui()
+        self.show()
+    
+    def initialize_ui(self):
+        """
+        Initialize the main window and display its contents of the widgets.
+        """
+        self.setGeometry(40, 40, 800, 900)
+        self.setWindowTitle("Grid Sytem Layout PyQt5")
+        self.display_content()
+    
+    
