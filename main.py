f553c15cd995e2aed0ea69ff94537ca0aa5abe17
khadada <lkhadada@gmail.com>
2022-04-16 07:55:59 +0100

    All done with the main window class of own gui

--- main.py
+++ main.py
@@ -18,4 +18,24 @@ class MainWindow(QMainWindow):
         self.setWindowTitle("Grid Sytem Layout PyQt5")
         self.display_content()
     
-    
+    def display_content(self):
+        """
+        Display the color in grid systm on the main window.
+        """
+        # Create grid layout:
+        grid_layout = QGridLayout()
+        # Create the container:
+        container = QWidget()
+        # Create color && add it to the grid layout:
+        grid_layout.addWidget(Color("Magenta"),0,0,1,3)
+        grid_layout.addWidget(Color("Yellow"),1,0,1,2)
+        grid_layout.addWidget(Color("Orange"),1,2,1,1)
+        grid_layout.addWidget(Color("Magenta"),2,0,1,3)
+        grid_layout.addWidget(Color("Teal"),3,0,1,1)
+        grid_layout.addWidget(Color("Pink"),3,1,1,2)
+        # Set the grid layout to the container:
+        container.setLayout(grid_layout)
+        # Set the container to be the central widget of the main window:
+        self.setCentralWidget(container)
+
+
